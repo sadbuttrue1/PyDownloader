2858cfb58fa61cd37ebe8da18d8f70ce32f27933
Eugene Aslanov <aslanov.ev@gmail.com>
2015-11-02 15:54:39 +0400

    Added some args

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from parameters import args
 __author__ = 'true'
 
 print(args.url)
+print(args.dir)
 if args.subdirs:
     for subdir in args.subdirs:
         print(subdir)
--- parameters.py
+++ parameters.py
@@ -4,5 +4,7 @@ __author__ = 'true'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('url', help='url that we want to download')
-parser.add_argument('--subdirs', nargs='+', help='set of subdirs')
+parser.add_argument('-d', '--dir', help='where we download (by default – in current script folder)')
+parser.add_argument('-s', '--subdirs', nargs='+', help='set of subdirs')
+parser.add_argument('-r', '--retries', help='count of retries (default 10)')
 args = parser.parse_args()
